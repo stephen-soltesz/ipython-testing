7faa832ca1c56c2d7aff2493b43ad67d68daa3cd
Stephen Soltesz <soltesz@google.com>
2018-03-09 16:54:26 -0500

    Extract short hostname from

--- daily_pct.sql
+++ daily_pct.sql
@@ -1,20 +1,19 @@
 select
-  hostname,
+  name as hostname,
   FORMAT_TIMESTAMP("%Y-%m-%d", TIMESTAMP_TRUNC(sts, DAY)) as day,
   UNIX_SECONDS(TIMESTAMP_TRUNC(sts, DAY)) as ts,
   countif(value > 0) / 8640 as total_discards
 FROM (
 
   SELECT
-    hostname,
+    REGEXP_EXTRACT(hostname, r'(mlab[1-4].[a-z]{3}[0-9]{2}).*') as name,
     sample.timestamp as sts,
     sample.value as value
-  
   from
     `mlab-sandbox.base_tables.switch*`,
     UNNEST(sample) as sample
   where
-        hostname like '%mlab1.dfw02%'
+        (hostname like '%mlab1.dfw02%' OR hostname like '%mlab1.lga03%')
     AND metric like 'switch.discards.uplink.tx'
   group by
     hostname, sts, value
